5b7f1ab972a9415656c30687098226c58b357751
BugHunter-X <askzenai@gmail.com>
2025-01-09 23:30:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,30 @@
+import * as Permissions from 'expo-permissions';
+import { Camera, CameraType } from 'expo-camera';
+import React, { useState, useEffect } from 'react';
+
+const CameraScreen = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(CameraType.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA);
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View><Text>Requesting permissions...</Text></View>;
+  }
+  if (hasPermission === false) {
+    return <View><Text>No access to camera</Text></View>;
+  }
+  return (
+    <View style={{ flex: 1 }}>
+      <Camera style={{ flex: 1 }} type={type}>
+        {/* Camera controls etc. */}
+      </Camera>
+    </View>
+  );
+};
+export default CameraScreen;
